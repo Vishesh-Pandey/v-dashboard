27b62952187f2f84369a4ecb4a5621b6975d55e8
Vishesh Pandey <74998585+Vishesh-Pandey@users.noreply.github.com>
2023-04-24 23:57:27 +0530

    add: profile for user

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -56,7 +56,8 @@ function Settings() {
       return (
         <div className="row">
           <div className="col-12 py-3 fw-bold">
-            <i className="bi bi-person-circle"></i> You are Logged in<i className="bi bi-wifi"></i>
+            <i className="bi bi-person-circle"></i> You are Logged in
+            <i className="bi bi-wifi"></i>
           </div>
           <div className="col">
             <button
@@ -74,13 +75,23 @@ function Settings() {
               Log out
             </button>
           </div>
+          <div className="col-12 py-2">
+            <button
+              data-bs-toggle="modal"
+              data-bs-target="#profileModal"
+              className="btn btn-primary w-100"
+            >
+              Profile
+            </button>
+          </div>
         </div>
       );
     } else {
       return (
         <div className="row">
           <div className="col-12 py-3 fw-bold">
-            <i className="bi bi-person-circle"></i>You are Offline<i className="bi bi-wifi-off"></i>
+            <i className="bi bi-person-circle"></i>You are Offline
+            <i className="bi bi-wifi-off"></i>
           </div>
 
           <div className="col-12">
@@ -105,7 +116,44 @@ function Settings() {
 
   return (
     <div className="row py-5">
-      <div className="col">{checkStatus()}</div>
+      <div className="col-12">{checkStatus()}</div>
+      <div className="col-12">
+        <div
+          className="modal fade"
+          id="profileModal"
+          tabIndex={-1}
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="exampleModalLabel">
+                  Profile
+                </h1>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                />
+              </div>
+              <div className="modal-body">
+                <p>Email: {getAuth().currentUser.email}</p>
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
